56091cb34cd80963f7d27699b5b6c3ce42c0f529
Bogdan Degtyariov <bogdan.degtyariov@percona.com>
2026-03-03 09:48:54 +0000

    Update script to install essentials

--- run_all.sh
+++ run_all.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
-VERSIONS=("10.11" "11.4" "12.1")
+sudo apt update
+sudo apt install docker.io sysstat sysbench mysql-client  -y
 
-for VERSION in "${VERSIONS[@]}"; do
-  ./run_metrics.sh "mariadb" "$VERSION"
-  echo ""
-done
+# VERSIONS=("10.11" "11.4" "12.1")
 
-VERSIONS=("5.7" "8.0" "8.4" "9.6")
+# for VERSION in "${VERSIONS[@]}"; do
+#   ./run_metrics.sh "mariadb" "$VERSION"
+#   echo ""
+# done
 
-for VERSION in "${VERSIONS[@]}"; do
-  ./run_metrics.sh "mysql" "$VERSION"
-  echo ""
-done
+# VERSIONS=("5.7" "8.0" "8.4" "9.6")
 
-VERSIONS=("5.7" "8.0" "8.4")
+# for VERSION in "${VERSIONS[@]}"; do
+#   ./run_metrics.sh "mysql" "$VERSION"
+#   echo ""
+# done
+
+# VERSIONS=("5.7" "8.0" "8.4")
+
+VERSIONS=("8.0")
 
 for VERSION in "${VERSIONS[@]}"; do
   ./run_metrics.sh "percona-server" "$VERSION"
